d1794ad7f977f4e9994f180d972caaca6c1dbf0b
trungcpt <trungcpt.work@gmail.com>
2025-11-09 21:56:00 +0700

    chore: update AppController test

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -16,7 +16,26 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World!222');
     });
   });
+
+  // describe('getHello', () => {
+  //   it('should return "Hello World!"', () => {
+  //     // Chuẩn bị data test, thêm kết quả mong đợi
+  //     const param1 = 2;
+  //     const param2 = 'abc';
+  //     const dataReturn = 'Hello World!';
+
+  //     // Gọi logic để test
+  //     const resultExpect = appController.getHello(param1, param2);
+
+  //     // Trả về kết quả khi test
+  //     expect(resultExpect).toBe(dataReturn);
+  //   });
+
+  //   it('should throw error when failed', () => {
+  //     expect(appController.getHello()).toThrow(Error);
+  //   });
+  // });
 });
