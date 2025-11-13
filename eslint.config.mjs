16db5c5e7585275ddcdfe13251410cb3b649e799
trungcpt <trungcpt.work@gmail.com>
2025-11-13 23:00:34 +0700

    chore: update error eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -46,6 +46,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-redundant-type-constituents': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-duplicate-type-constituents': 'off',
     },
   },
 );
